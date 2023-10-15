d4818b94e4318e9d8c0fa01f072bafa492b82446
yamataikoku0114 <127322427+yamataikoku0114@users.noreply.github.com>
2023-10-15 18:45:23 +0900

    記事詳細画面の内容を真ん中に表示するためのCSS追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Header />
-          {children}
+          <div className="flex justify-center">{children}</div>
           <Footer />
         </ThemeProvider>
       </body>
