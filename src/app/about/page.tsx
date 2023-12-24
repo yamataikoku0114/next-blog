0c59ca717c2c1628e1e4acecb276d001be57794f
yamataikoku0114 <127322427+yamataikoku0114@users.noreply.github.com>
2023-12-24 16:26:22 +0900

    自己紹介を修正しました。

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,17 +7,17 @@ export default function About() {
       <div>
         <div className="flex justify-center mt-4">
           <Image
-            src="/images/Pasa.png"
+            src="/images/introductionpicture.png"
             alt="自己紹介画像"
             width={300}
             height={300}
           />
         </div>
-        <div className="text-3xl font-bold mt-4">Yamamoto Taiki</div>
-        <div>age:</div>
-        <div>経歴:</div>
-        <div>説明:</div>
-        <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        <div className="text-3xl font-bold mt-4">
+          山本 泰暉（Yamamoto Taiki）
+        </div>
+        <div>年齢：22歳</div>
+        <div>経歴：社会人1年目のWebエンジニアです。</div>
       </div>
     </div>
   );
