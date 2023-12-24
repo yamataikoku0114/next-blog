a9c97a787c0195dc902faee37200ad74b719e532
yamataikoku0114 <127322427+yamataikoku0114@users.noreply.github.com>
2023-12-24 15:12:27 +0900

    Home画面の仮実装を削除し、実際の記事の一覧が表示されるように修正しました。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,21 @@
+import { getData } from '../../lib/getData';
+import { PostCard } from './components/PostCard';
+
 export default function Home() {
+  const allPosts = getData();
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">最近の投稿</h1>
-      <div>
-        <div className="bg-white p-4 shadow-md rounded-md mb-4 dark:bg-slate-500">
-          <h2>投稿タイトル1</h2>
-          <p>タグ</p>
-          <p>投稿詳細</p>
-          <p>投稿日時:</p>
+      {allPosts && (
+        <div>
+          {allPosts.map((post) => (
+            <div key={post.slug}>
+              <PostCard post={post} />
+            </div>
+          ))}
         </div>
-        <div className="bg-white p-4 shadow-md rounded-md mb-4 dark:bg-slate-500">
-          <h2>投稿タイトル1</h2>
-          <p>タグ</p>
-          <p>投稿詳細</p>
-          <p>投稿日時:</p>
-        </div>
-        <div className="bg-white p-4 shadow-md rounded-md mb-4 dark:bg-slate-500">
-          <h2>投稿タイトル1</h2>
-          <p>タグ</p>
-          <p>投稿詳細</p>
-          <p>投稿日時:</p>
-        </div>
-        <div className="bg-white p-4 shadow-md rounded-md mb-4 dark:bg-slate-500">
-          <h2>投稿タイトル1</h2>
-          <p>タグ</p>
-          <p>投稿詳細</p>
-          <p>投稿日時:</p>
-        </div>
-        <div className="bg-white p-4 shadow-md rounded-md mb-4 dark:bg-slate-500">
-          <h2>投稿タイトル1</h2>
-          <p>タグ</p>
-          <p>投稿詳細</p>
-          <p>投稿日時:</p>
-        </div>
-      </div>
+      )}
+      <div></div>
     </div>
   );
 }
