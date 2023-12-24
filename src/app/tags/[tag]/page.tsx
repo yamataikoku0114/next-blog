890e9b43a37ca541fe6a50a1034ebd6010e8ea7a
yamataikoku0114 <127322427+yamataikoku0114@users.noreply.github.com>
2023-12-24 09:18:17 +0900

    記事名が表示されている箇所を削除しました。

--- src/app/tags/[tag]/page.tsx
+++ src/app/tags/[tag]/page.tsx
@@ -5,16 +5,7 @@ export default function TagLists({ params }: { params: { tag: string } }) {
   return (
     <div className="p-4">
       <div className="text-2xl font-bold mb-4">タグ一覧</div>
-      <div className="flex flex-wrap">
-        {posts.map((post, index) => (
-          <button
-            key={index}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full m-2"
-          >
-            {post.title}
-          </button>
-        ))}
-      </div>
+      <div className="flex flex-wrap"></div>
     </div>
   );
 }
