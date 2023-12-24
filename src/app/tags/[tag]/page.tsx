e54ab3193d58cab59348671d981eb71f19cf77f0
yamataikoku0114 <127322427+yamataikoku0114@users.noreply.github.com>
2023-12-24 09:29:56 +0900

    タグで絞り込んだ時の記事一覧でPostCardコンポーネントを使用するようにしました。

--- src/app/tags/[tag]/page.tsx
+++ src/app/tags/[tag]/page.tsx
@@ -1,11 +1,22 @@
 import { getPostsByTag } from '../../../../lib/getPostsByTag';
+import { PostCard } from '@/app/components/PostCard';
 
 export default function TagLists({ params }: { params: { tag: string } }) {
   const posts = getPostsByTag(params.tag);
   return (
     <div className="p-4">
-      <div className="text-2xl font-bold mb-4">タグ一覧</div>
-      <div className="flex flex-wrap"></div>
+      <main>
+        <h1 className="text-2xl font-bold mb-4">記事一覧</h1>
+        {posts && (
+          <div>
+            {posts.map((post) => (
+              <div key={post.slug}>
+                <PostCard post={post} />
+              </div>
+            ))}
+          </div>
+        )}
+      </main>
     </div>
   );
 }
